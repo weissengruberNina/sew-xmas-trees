97819d20b09fb79f68da351abfa811667b349521
StarkaLukas <lstarka141000@gmail.com>
2021-11-29 14:24:57 +0100

    add route for tree

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {TreeComponent} from "./tree/tree.component";
 
 const routes: Routes = [
+    {path: 'tree', component: TreeComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
 
