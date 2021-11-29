a686e10f4b24fa581c4d1cc8f404bc7dbdddbb00
StarkaLukas <lstarka141000@gmail.com>
2021-11-29 14:13:24 +0100

    add mat-card with title

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
