2bc51913100bc40525d5579adbfe8c9fb6f7da2c
StarkaLukas <lstarka141000@gmail.com>
2021-12-06 13:56:48 +0100

    return Observable

--- frontend/src/app/shared/backend.service.ts
+++ frontend/src/app/shared/backend.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {firstValueFrom} from "rxjs";
+import {firstValueFrom, Observable} from "rxjs";
 
 const BASE_URL: string = 'http://localhost:8080/tree'
 
@@ -11,9 +11,8 @@ export class BackendService {
 
   constructor(private readonly http: HttpClient) { }
 
-  public getAllTreeTypes(): Promise<TreeType[]> {
-    const res = this.http.get<TreeType[]>(`${BASE_URL}/types`);
-    return firstValueFrom(res);
+  public getAllTreeTypes(): Observable<TreeType[]> {
+    return this.http.get<TreeType[]>(`${BASE_URL}/types`);
   }
 }
 
