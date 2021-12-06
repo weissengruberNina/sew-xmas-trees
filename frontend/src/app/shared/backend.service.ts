8d09d6cdfd07bff3f222f916bd70fa48ccb3a161
StarkaLukas <lstarka141000@gmail.com>
2021-12-06 13:11:36 +0100

    create interface and enum

--- frontend/src/app/shared/backend.service.ts
+++ frontend/src/app/shared/backend.service.ts
@@ -8,3 +8,16 @@ export class BackendService {
 
   constructor(private readonly http: HttpClient) { }
 }
+
+export enum TreeType {
+  Blaufichte,
+  Nordmanntanne
+}
+
+export interface ITree {
+  id: number,
+  type: TreeType,
+  heightMeters: number,
+  price: number,
+  sold: boolean
+}
