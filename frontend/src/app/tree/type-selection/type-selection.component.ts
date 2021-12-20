8972f88f672aca3096a94534cda87c1c013e5636
StarkaLukas <lstarka141000@gmail.com>
2021-12-20 13:15:52 +0100

    add selectedType variable

--- frontend/src/app/tree/type-selection/type-selection.component.ts
+++ frontend/src/app/tree/type-selection/type-selection.component.ts
@@ -1,29 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BackendService} from "../../shared/backend.service";
 
 @Component({
-  selector: 'app-type-selection',
-  templateUrl: './type-selection.component.html',
-  styleUrls: ['./type-selection.component.scss']
+    selector: 'app-type-selection',
+    templateUrl: './type-selection.component.html',
+    styleUrls: ['./type-selection.component.scss']
 })
 export class TypeSelectionComponent implements OnInit {
 
-  public treeTypes: string[] | null;
+    public treeTypes: string[] | null;
+    public selectedType: string | null;
 
-  constructor(private readonly backend: BackendService) {
-    this.treeTypes = null;
-  }
+    constructor(private readonly backend: BackendService) {
+        this.treeTypes = null;
+        this.selectedType = null;
+    }
 
-  public ngOnInit(): void {
-    this.backend.getAllTreeTypes()
-      .subscribe({
-        next: res => {
-            this.treeTypes = res;
-        },
-        error: err => {
-            console.log(err.messageText)
-        }
-      });
-  }
+    public ngOnInit(): void {
+        this.backend.getAllTreeTypes()
+            .subscribe({
+                next: res => {
+                    this.treeTypes = res;
+                },
+                error: err => {
+                    console.log(err.messageText)
+                }
+            });
+    }
 
 }
