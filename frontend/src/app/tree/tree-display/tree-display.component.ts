556bdb5c4c9099f60a545da422e9007947b4779d
StarkaLukas <lstarka141000@gmail.com>
2021-12-20 14:24:41 +0100

    implement tree-display

--- frontend/src/app/tree/tree-display/tree-display.component.ts
+++ frontend/src/app/tree/tree-display/tree-display.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {BackendService, ITree} from "../../shared/backend.service";
 
 @Component({
-  selector: 'app-tree-display',
-  templateUrl: './tree-display.component.html',
-  styleUrls: ['./tree-display.component.scss']
+    selector: 'app-tree-display',
+    templateUrl: './tree-display.component.html',
+    styleUrls: ['./tree-display.component.scss']
 })
 export class TreeDisplayComponent implements OnInit {
 
-  constructor() { }
+    @Input()
+    public treeId!: number;
 
-  ngOnInit(): void {
-  }
+    @Input()
+    public type!: string;
+
+    public tree: ITree | null;
+
+    public constructor(private readonly backendService: BackendService) {
+        this.tree = null;
+    }
+
+    public async ngOnInit(): Promise<void> {
+        this.tree = await this.backendService.getTreeById(this.type, this.treeId);
+    }
+
+    public get btnText(): string {
+        return this.tree?.sold ? 'Sold' : 'Buy';
+    }
 
 }
