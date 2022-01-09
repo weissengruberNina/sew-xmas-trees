b9950a37f6a911ffbe5387c4328fb8dc60e5025e
weissengruberNina <weissengrubernina@gmail.com>
2022-01-09 18:06:56 +0100

    create sell-data.service.ts

---
+++ frontend/src/app/shared/sell-data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {ITree} from "./backend.service";
+@Injectable({
+    providedIn: 'root'
+})
+export class SaleDataService {
+    private treeToSell: ITree | null;
+    public constructor() {
+        this.treeToSell = null
+    }
+    public set tree(tree: ITree | null){
+        this.treeToSell = tree;
+    }
+    public get tree(): ITree | null {
+        return this.treeToSell;
+    }
+}
